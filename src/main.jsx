c946e8d4f25069099a9390e371959f5824a23da0
Nikitin <nikitinigor0907@gmail.com>
2024-11-06 12:37:38 +0100

    created

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import "modern-normalize";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
